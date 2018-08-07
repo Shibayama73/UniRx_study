7b0795c77c061c8352f6d97b9ec5acd9a753ae8e
wave <n3wtk69@gmail.com>
2018-08-07 17:23:30 +0900

    UniRx勉強用

---
+++ Assets/Script/MinEnemyMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MinEnemyMove : AttackBase {
+
+    //目的位置
+    public GameObject _targetObj;
+    //ナビメッシュ
+    NavMeshAgent _navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+        // NavMeshAgentコンポーネント取得
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //NavMeshAgentが準備できているなら
+        if (_navMeshAgent.pathStatus != NavMeshPathStatus.PathInvalid)
+        {
+            //目的地をセット
+            _navMeshAgent.SetDestination(_targetObj.transform.position);
+            _navMeshAgent.updatePosition = true;
+            _navMeshAgent.updateRotation = true;
+        }
+
+    }
+
+    /// <summary>
+    /// 停止する
+    /// </summary>
+    /// <returns></returns>
+    public void Stop()
+    {
+        //Debug.Log("STOP");
+        _navMeshAgent.updatePosition = false;
+        //_navMeshAgent.updateRotation = false;
+    }
+
+}
---
+++ Assets/Script/PlayerTargetMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class PlayerTargetMove : MoveBase {
+
+    public float _speed = 1.0f; // 移動速度 m/s
+
+    // Use this for initialization
+    void Start()
+    {
+        // Observable.Return(new Vector3(0, 10, 0)).Subscribe(v => gameObject.transform.position = v);
+
+        //更新(GameObjectが破棄されたら勝手に停止するため安全)
+        this.UpdateAsObservable()
+            //キーが押されたら(Where:条件)
+            .Where(_ => Input.GetKey(KeyCode.UpArrow))
+            // 移動する(Subscribe:処理の登録)
+            .Subscribe(l => MoveUp(_speed));
+
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetKey(KeyCode.DownArrow))
+            .Subscribe(l => MoveDown(_speed));
+
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetKey(KeyCode.RightArrow))
+            .Subscribe(l => MoveRight(_speed));
+
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetKey(KeyCode.LeftArrow))
+            .Subscribe(l => MoveLeft(_speed));
+    }
+	
+    //↓UniRx導入前
+	// Update is called once per frame
+    //void Update()
+    //{
+    //    Vector3 pos = transform.position;
+    //    float moveLength = Speed * Time.smoothDeltaTime;
+    //    if (Input.GetKey(KeyCode.UpArrow))
+    //    {
+    //        pos.z += moveLength;
+    //    }
+    //    else if (Input.GetKey(KeyCode.DownArrow))
+    //    {
+    //        pos.z -= moveLength;
+    //    }
+    //    else if (Input.GetKey(KeyCode.LeftArrow))
+    //    {
+    //        pos.x -= moveLength;
+    //    }
+    //    else if (Input.GetKey(KeyCode.RightArrow))
+    //    {
+    //        pos.x += moveLength;
+    //    }
+    //    transform.position = pos;
+    //}
+
+}
